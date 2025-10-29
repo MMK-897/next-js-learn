17b708ca707fb61b2ad64c90c8bcf0127f18c704
Mohamed Kolleh Mansaray <mohamedkolleh20@gmail.com>
2025-10-29 14:10:30 -0800

    fix the errors for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
-import { Lusitana} from 'next/font/google';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
- 
-export const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "My App",
+  description: "Description goes here",
+};
 
 export default function RootLayout({
   children,
@@ -12,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`} >{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
