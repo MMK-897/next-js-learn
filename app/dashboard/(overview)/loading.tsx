27fac9630f9f1478214dc4a99d136eb2125b3d86
Mohamed Kolleh Mansaray <mohamedkolleh20@gmail.com>
2025-11-18 12:07:02 +0000

    Added a loading skeleton in the dashboard page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons'
+
+const Loading = () => {
+  return (
+    <div><DashboardSkeleton/></div>
+  )
+}
+
+export default Loading
